bbf52bd22aab6f53e06091610f938b2a9e3d1d51
tsahiBarshevsky <tsahi.13@gmail.com>
2021-10-22 09:43:57 +0300

    Basic react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default App;
